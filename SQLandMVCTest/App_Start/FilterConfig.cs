96cf9d7d47adc45db39a71a448b708c25cd2ec32
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-10-22 11:40:55 +0300

    Add project files.

---
+++ SQLandMVCTest/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SQLandMVCTest
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
