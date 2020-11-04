35b5a859aec607f6def2e905e81138a0930736cf
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-11-04 12:32:22 +0300

    Customer table in DB created

---
+++ MVCDataBase/dbo/Tables/CustomersTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CustomersTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CustomerID] INT NOT NULL, 
+    [FullName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(255) NOT NULL
+
+)
