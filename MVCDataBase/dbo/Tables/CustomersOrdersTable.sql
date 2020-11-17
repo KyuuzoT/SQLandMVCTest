04d0f4d97ed0c91e78eed3da887ed0bf9eaec817
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-11-17 10:56:53 +0300

    Deleting CustomerOrdersTable

--- MVCDataBase/dbo/Tables/CustomersOrdersTable.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[CustomersOrdersTable]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [CustomerId] INT REFERENCES CustomersTable(Id) NOT NULL,
-	[OrderId] INT REFERENCES OrdersTable(Id) NOT NULL
-)
